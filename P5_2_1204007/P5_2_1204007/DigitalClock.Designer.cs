ab60fb74babcabeba31cc06b86cd07ea96be5f1d
Wulan Nur Annisah <wulannurannisah470@gmail.com>
2021-12-09 10:31:35 +0700

    Add files via upload

---
+++ P5_2_1204007/P5_2_1204007/DigitalClock.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace P5_2_1204007
+{
+    partial class DigitalClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LocalTimeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Timer1
+            // 
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick_1);
+            // 
+            // LocalTimeLabel
+            // 
+            this.LocalTimeLabel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.LocalTimeLabel.Location = new System.Drawing.Point(28, 37);
+            this.LocalTimeLabel.Name = "LocalTimeLabel";
+            this.LocalTimeLabel.Size = new System.Drawing.Size(140, 23);
+            this.LocalTimeLabel.TabIndex = 0;
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LocalTimeLabel);
+            this.Name = "DigitalClock";
+            this.Size = new System.Drawing.Size(197, 95);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label LocalTimeLabel;
+    }
+}
